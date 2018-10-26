989df67767e0f22c5863e2c3ba070dfc48ea7cde
GR8 <gr8_boi52@yahoo.com>
2018-10-26 16:10:35 +0400

    pre defined clans

--- clashroyale.sikuli/clashroyale.py
+++ clashroyale.sikuli/clashroyale.py
@@ -2,6 +2,8 @@ import shutil
 import os
 import time
 
+clans = [ "#8V8CC2", "#2999R82R", "#PPC8QPY9", "#89GLVGGC", "#98QYCVVQ", "#QRCVRJY", "#8RVVU8RY", "#2QJR8G2G", "#8V2V2GLJ", "#UJGR90R", "#29RR9Q00", "#80JR9CY8", "#2LG00R", "#9RVPU8CC", "#9GJLUJP2", "#2VP2UP", "#JUGYLQG", "#PVUJQQ9", "#9CYYJR0G", "#98P22CYQ", "#YCVUR2", "#99PRPCJ2", "#9CYYC2RC", "#8Y2P0Y8", "#P88P9VRY", "#2QPVJ0L9", "#9PURUQGL", "#9UJRCC08", "#JYYQ0G", "#9CY92C92", "#CQJY8RP", "#9JP2CL2Y", "#PU988Q0", "#9ULPC080", "#292J8QV", "#9JL0ULC9", "#2JV0VL8", "#99GJJJGC", "#9PVY28R", "#2VUVU2U", "#P0GCGGJC", "#2V9C0YYQ", "#RPLQV2P", "#8290GQ90", "#8YCCVU0", "#G2PJURR", "#82PRP29C", "#PL299P", "#CL8CP0", "#P8GYCGYR", "#LGRJ9G", "#8GGRG0VY", "#8GLGPPLL", "#8R9JY9UC", "#JRCQRY" ]
+
 r = App.focusedWindow()
 r2 = Region(r.x, r.y + 600, r.w, r.h - 600)
 r.setAutoWaitTimeout(2)
@@ -63,8 +65,11 @@ def leave_clan():
     r.click("LeaveButton.png")
     r.click("YesButton.png")
         
-while r.exists("ShopTab.png"):
+for clan in clans:
     if r.exists("SearchButton.png"):
+        r.type("1540555777035.png" ,clan)
+        r.click("1540555791644.png")
+        wait(1)
         r.click("YellowTrophy.png")
         if r.exists("JoinButton.png"):
             r.click("JoinButton.png")
